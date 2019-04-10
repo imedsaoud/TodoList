25ca70d921242c8a338afb99f96583ab4c8f5ad7
imedsa <contact.imedsa@gmail.com>
2019-04-11 01:59:32 +0200

    Rename column

--- db/Db_todos.sql
+++ db/Db_todos.sql
@@ -3,19 +3,22 @@ CREATE DATABASE todos ;
 USE todos;
 
 CREATE TABLE todo (
-	todo_id INT UNSIGNED AUTO_INCREMENT,
-	todo_task VARCHAR(50) NOT NULL,
-	todo_url VARCHAR(255) NOT NULL,
-	todo_category VARCHAR (50) NOT NULL,
-  todo_priority VARCHAR(50) NOT NULL,
-	todo_status VARCHAR(50) NOT NULL,
-	PRIMARY KEY(todo_id)
+	id INT UNSIGNED AUTO_INCREMENT,
+	task VARCHAR(50) NOT NULL,
+	url VARCHAR(255) NOT NULL,
+	category VARCHAR (50) NOT NULL,
+  priority VARCHAR(50) NOT NULL,
+	status VARCHAR(50) NOT NULL,
+	PRIMARY KEY(id)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-INSERT INTO `todo` (`todo_task`, `todo_url`, `todo_category`, `todo_priority`,`todo_status`) VALUES
+INSERT INTO `todo` (`task`, `url`, `category`, `priority`,`status`) VALUES
 ('Logique formulaire', 'https://github.com/imedsaoud/FightBlog', 'PSR', 'High','Todo');
 
 
-INSERT INTO `todo` (`todo_task`, `todo_url`, `todo_category`, `todo_priority`,`todo_status`) VALUES
+INSERT INTO `todo` (`task`, `url`, `category`, `priority`,`status`) VALUES
 ('Amelioration chemin', 'https://github.com/imedsaoud/Routeur', 'Maths', 'Low','Need Review');
+
+INSERT INTO `todo` (`task`, `url`, `category`, `priority`,`status`) VALUES
+('Pattern update', 'https://github.com/imedsaoud/Routeur', 'Design Patterns', 'Medium','Done');

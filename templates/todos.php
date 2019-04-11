272d1d9c9be778bde8a337b4fdb7e7501e258f41
imedsa <contact.imedsa@gmail.com>
2019-04-11 02:03:49 +0200

    Todo view

--- templates/todos.php
+++ templates/todos.php
@@ -1,14 +1,24 @@
 
+<?php
+    foreach ($dataTodos["content"] as $data){
 
-<ul class = "todos">
-    <li class = "todo todo__task"><?=$_POST["todo_content"]?> </li>
-    <li class = "todo todo__url"><?=$_POST["todo_git"]?></li>
-    <li class = "todo todo__priority"><?=$_POST["todo_priority"]?></li>
-    <li class = "todo todo__status"><?=$_POST["todo_status"]?></li>
-    <form class="delete-todo" action="/delete" method="post">
-        <input id="delete_todo" class="hidden" type="submit">
-        <label for="delete_todo"><img src="https://image.flaticon.com/icons/svg/63/63260.svg" width="28px"></label>
-    </form>
-    </li>
-</ul>
+
+   echo     ' 
+                <ul class = "todos">
+                     <li class = "todo todo__task">'.$data["task"].'</li>
+                     <li class = "todo todo__url">'.$data["url"].'</li>
+                     <li class = "todo todo__priority">'.$data["priority"].'</li>
+                     <li class = "todo todo__status">'.$data["status"].'</li>
+                     <form class="delete-todo" action="/delete" method="post">
+                        <input id="delete_todo" class="hidden" type="submit">
+                        <label for="delete_todo"><img src="https://image.flaticon.com/icons/svg/63/63260.svg" width="28px"></label>
+                     </form>
+                 </ul>
+             ';
+    }
+?>
+
+
+
+
 
